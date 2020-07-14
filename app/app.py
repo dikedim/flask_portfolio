8148b0694a0b17be73fed76b4bbc39088251677d
dikedim <dike.dim@outlook.com>
2020-07-14 14:37:40 +0100

    Added base working template

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
-from views import *
-from mobile_porfolio.mobile import mobile_bp
-from photography.photo import photo_bp
-from web_portfolio.web import web_bp
+#from views import *
+from .mobile.mobile import mobile_bp
+from .photography.photo import photo_bp
+from .web.web import web_bp
+from .home.main import home_bp
 
 
+#app = Flask(__name__, static_url_path="")
 app = Flask(__name__)
 app.register_blueprint(mobile_bp)
 app.register_blueprint(photo_bp)
 app.register_blueprint(web_bp)
+app.register_blueprint(home_bp)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- views.py
+++ views.py
@@ -4,19 +4,19 @@ from flask import render_template, url_for, redirect, request
 
 @app.route('/error_404')
 def error_404():
-    return render_template("error_404.html")
+    return render_template("app/home/templates/error_404.html")
 
 
 @app.route('/post')
 def blog_post():
-    return render_template("blog-post.html")
+    return render_template("app/home/templates/blog-post.html")
 
 
 @app.route('/blog')
 def blog():
-    return render_template("blog.html")
+    return render_template("app/home/templates/blog.html")
 
 
-@app.route('/')
-def index():
-    return render_template("index-bgcolor.html")
+#@app.route('/')
+#def index():
+#    return render_template("index-bgcolor.html")
