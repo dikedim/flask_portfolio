f6b63fc491b4ebadec0fdedb18006c4dcb7da15f
dikedim <dike.dim@outlook.com>
2021-03-19 00:29:00 +0100

    Started work on admin dashboard

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -8,17 +8,41 @@ def index():
     return render_template("admin/index.html")
 
 
-@admin.route('/login', methods=['GET'])
-def index():
+@admin.route('/admin/login', methods=['GET'])
+def login():
     return render_template("admin/login.html")
 
 
-@admin.route('/register', methods=['GET'])
-def index():
+@admin.route('/admin/register', methods=['GET'])
+def register():
     return render_template("admin/register.html")
 
 
 @admin.route('/logout', methods=['GET'])
-def index():
+def logout():
     return render_template("admin/index.html")
 
+
+@admin.route('/admin/mail', methods=['GET'])
+def mail():
+    return render_template("admin/mailbox.html")
+
+
+@admin.route('/admin/compose', methods=['GET'])
+def mail_compose():
+    return render_template("admin/mailbox-compose.html")
+
+
+@admin.route('/admin/inbox', methods=['GET'])
+def inbox():
+    return render_template("admin/mailbox-view.html")
+
+
+@admin.route('/admin/lock', methods=['GET'])
+def lock():
+    return render_template("admin/lock.html")
+
+
+@admin.route('/admin/register', methods=['GET'])
+def password_recovery():
+    return render_template("admin/register.html")
